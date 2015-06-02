7862850cd89009b4a4a16873fc58920131a589af
cjmatson <matson.cj@gmail.com>
2015-06-02 13:29:53 -0500

    Added a time indicator for song progress

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -374,9 +374,16 @@ require.register("scripts/album", function(exports, require, module) {
 
    blocJams.controller('PlayerBar.controller', ['$scope', 'SongPlayer', function($scope, SongPlayer) {
    $scope.songPlayer = SongPlayer;
+
+   SongPlayer.onTimeUpdate(function(event, time){
+     $scope.$apply(function(){
+       $scope.playTime = time;
+     });
+   });
+
  }]);
 
-  blocJams.service('SongPlayer', function() {
+  blocJams.service('SongPlayer', ['$rootScope', function($rootScope) {
     var currentSoundFile = null;
     var trackIndex = function(album, song) {
      return album.songs.indexOf(song);
@@ -419,22 +426,29 @@ require.register("scripts/album", function(exports, require, module) {
          currentSoundFile.setTime(time);
        }
      },
+     onTimeUpdate: function(callback) {
+      return $rootScope.$on('sound:timeupdate', callback);
+    },
      setSong: function(album, song) {
       if (currentSoundFile) {
       currentSoundFile.stop();
     }
        this.currentAlbum = album;
        this.currentSong = song;
+
        currentSoundFile = new buzz.sound(song.audioUrl, {
        formats: [ "mp3" ],
        preload: true
-    });
- 
+      });
+
+       currentSoundFile.bind('timeupdate', function(e){
+        $rootScope.$broadcast('sound:timeupdate', this.getTime());
+      });
+
     this.play();
      }
    };
- });
-
+ }]);
 
  blocJams.directive('slider', ['$document', function($document){
   // Returns a number between 0 and 1 to determine where the mouse event happened along the slider bar.
@@ -462,7 +476,7 @@ require.register("scripts/album", function(exports, require, module) {
    }
 
    return {
-     templateUrl: '/templates/directives/slider.html', // We'll create these files shortly.
+     templateUrl: '/templates/directives/slider.html', 
      replace: true,
      restrict: 'E',
      scope: {
@@ -525,6 +539,35 @@ require.register("scripts/album", function(exports, require, module) {
       }
     };
   }]);
+
+  blocJams.filter('timecode', function(){
+   return function(seconds) {
+     seconds = Number.parseFloat(seconds);
+ 
+     // Returned when no time is provided.
+     if (Number.isNaN(seconds)) {
+       return '-:--';
+     }
+ 
+     // make it a whole number
+     var wholeSeconds = Math.floor(seconds);
+ 
+     var minutes = Math.floor(wholeSeconds / 60);
+ 
+     remainingSeconds = wholeSeconds % 60;
+ 
+     var output = minutes + ':';
+ 
+     // zero pad seconds, so 9 seconds should be :09
+     if (remainingSeconds < 10) {
+       output += '0';
+     }
+ 
+     output += remainingSeconds;
+ 
+     return output;
+   }
+ })
 
 
 });
